a1fb74fa8128df39fbd0123561edca44f08de2ce
Omar Sebouai <omar.sebouai@gmail.com>
2019-04-12 16:21:16 +0200

    added project

---
+++ build/precache-manifest.ae0fa872ecb1588e57114845773c1f41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "290c4fc895107e396540",
+    "url": "/static/css/main.34e2004e.chunk.css"
+  },
+  {
+    "revision": "290c4fc895107e396540",
+    "url": "/static/js/main.31e487e7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "cf4e15b748306db01928db1f5582e743",
+    "url": "/index.html"
+  }
+];
